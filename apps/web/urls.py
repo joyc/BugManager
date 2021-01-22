c7df8c44606c0d41008ba59beed6443fd480df53
joyc <joyc@live.jp>
2021-01-23 01:46:35 +0900

    manage urls

---
+++ apps/web/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File   : urls.py
+# @Author : Hython
+# @Date   : 公元 2021/01/23 1:26
+from django.urls import path
+from web.views import home, account
+
+urlpatterns = [
+    path('', home.index),
+    path('login/', account.user_login),
+]
--- apps/web/views/account.py
+++ apps/web/views/account.py
@@ -3,3 +3,8 @@
 # @File   : account.py
 # @Author : Hython
 # @Date   : 公元 2021/01/23 1:21
+from django.shortcuts import render, HttpResponse
+
+
+def user_login(request):
+    return HttpResponse("登陆页面")
